ea6200c8c7bd80aa6cdedb4c7b7b883994a108a4
ChrisGarey <vinylogic@icloud.com>
2022-07-07 19:34:22 -0400

    first basic run of code to check python version

---
+++ python_version.py
@@ -0,0 +1,10 @@
+# A python program to get the version of python I am using
+# Import system module from Python.
+
+import sys
+
+
+input('Please press [Enter] to check system Python Version. ')
+print('Checking python version...')
+print('You are using\n' + sys.version)
+print(sys.version_info)
